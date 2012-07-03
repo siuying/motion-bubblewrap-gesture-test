914112a49f4ef9135fe681b1312c9093d61774ac
Francis Chong <francis@ignition.hk>
2012-07-03 21:25:13 +0800

    use gesture recognizer

--- app/timer_controller.rb
+++ app/timer_controller.rb
@@ -1,3 +1,43 @@
+# Opens UIView to add methods for working with gesture recognizers.
+
+class UIView
+
+  def whenTapped(enableInteraction=true, &proc)
+    addGestureRecognizerHelper(proc, enableInteraction, UITapGestureRecognizer.alloc.initWithTarget(proc, action: :'call:'))
+  end
+
+  def whenPinched(enableInteraction=true, &proc)
+    addGestureRecognizerHelper(proc, enableInteraction, UIPinchGestureRecognizer.alloc.initWithTarget(proc, action:'call'))
+  end
+
+  def whenRotated(enableInteraction=true, &proc)
+    addGestureRecognizerHelper(proc, enableInteraction, UIRotationGestureRecognizer.alloc.initWithTarget(proc, action:'call'))
+  end
+
+  def whenSwiped(enableInteraction=true, &proc)
+    addGestureRecognizerHelper(proc, enableInteraction, UISwipeGestureRecognizer.alloc.initWithTarget(proc, action:'call'))
+  end
+
+  def whenPanned(enableInteraction=true, &proc)
+    addGestureRecognizerHelper(proc, enableInteraction, UIPanGestureRecognizer.alloc.initWithTarget(proc, action:'call'))
+  end
+
+  def whenPressed(enableInteraction=true, &proc)
+    addGestureRecognizerHelper(proc, enableInteraction, UILongPressGestureRecognizer.alloc.initWithTarget(proc, action:'call'))
+  end
+
+  private
+
+  # Adds the recognizer and keeps a strong reference to the Proc object.
+  def addGestureRecognizerHelper(proc, enableInteraction, recognizer)
+    setUserInteractionEnabled true if enableInteraction && !isUserInteractionEnabled
+    self.addGestureRecognizer(recognizer)
+    @recognizers = {} unless @recognizers
+    @recognizers["#{proc}"] = proc
+  end
+
+end
+
 class TimerController < UIViewController
   def viewDidLoad
     margin = 20
@@ -15,6 +55,11 @@ class TimerController < UIViewController
     @action.setTitle('Start', forState:UIControlStateNormal)
     @action.setTitle('Stop', forState:UIControlStateSelected)
     @action.addTarget(self, action:'actionTapped', forControlEvents:UIControlEventTouchUpInside)
+    @action.whenTapped do |gesture_recognizer|
+      puts "gesture_recognizer: #{gesture_recognizer}"
+      actionTapped
+    end
+
     @action.frame = [[margin, 260], [view.frame.size.width - margin * 2, 40]]
     view.addSubview(@action)
   end
